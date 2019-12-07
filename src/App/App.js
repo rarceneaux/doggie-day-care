cd0ef0d1337a8f89e74b2fe9e0a40afbd3344132
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-12-07 16:53:25 -0600

    setup done

--- src/App/App.js
+++ src/App/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button className='btn btn-success'>The LAST HU$TLE IS TECHNOLOGY™</button>
         <a
           className="App-link"
           href="https://reactjs.org"
